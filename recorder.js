b5fc5c509fabd08f2c2bc00814dbff852e1d1b26
nibral <nibral@dancingdtarson.me>
2015-01-26 07:25:14 +0000

    Add:Get stream URL from getfmsListHD.php

--- recorder.js
+++ recorder.js
@@ -1,4 +1,6 @@
-var agqrStreamUrl = 'rtmp://fms-base1.mitene.ad.jp/agqr/aandg11';
+var http = require('http');
+var url = 'http://www.uniqueradio.jp/agplayerf/getfmsListHD.php';
+var parseString = require('xml2js').parseString;
 
 var fs = require('fs');
 var exec = require('child_process').exec;
@@ -32,15 +34,39 @@ var encode = function(filename) {
 }
 
 exports.record = function(filename, length) {
-    var flvPath = '"' + outputDir + '/' + filename + '.flv"';
-    var lengthSec = length * 60 + 15;
-    var recordCommand = rtmpdump + ' -r ' + agqrStreamUrl + ' --live -B ' + lengthSec + ' -o ' + flvPath;
-    exec(recordCommand, {maxBuffer: 1*1024*1024}, function(err, stdout, stderr) {
-        if(err) throw err;
-        console.log('finish recode:' + flvPath);
-        encode(filename);
+  http.get(url, function(res) {
+    var body = '';
+
+    res.on('data', function(chunk) {
+      body += chunk;
     });
-    console.log('start recode:' + flvPath);
+
+    res.on('end', function(res) {
+      parseString(body, function(err, result){
+        if(err) {
+          console.log(err);
+        }
+
+        var serverinfo = result.ag.serverlist[0].serverinfo[0];
+        var server = serverinfo.server[0].match(/^.*(rtmp.*)$/)[1];
+        var app = serverinfo.app[0];
+        var stream = serverinfo.stream[0];
+        var agqrStreamUrl = server + '/' + app + '/' + stream;
+
+        var flvPath = '"' + outputDir + '/' + filename + '.flv"';
+        var lengthSec = length * 60 + 15;
+        var recordCommand = rtmpdump + ' -r ' + agqrStreamUrl + ' --live -B ' + lengthSec + ' -o ' + flvPath;
+        exec(recordCommand, {maxBuffer: 1*1024*1024}, function(err, stdout, stderr) {
+          if(err) throw err;
+          console.log('finish recode:' + flvPath);
+          encode(filename);
+        });
+        console.log('start recode:' + flvPath);
+      });
+    });
+  }).on('error', function(err) {
+    console.log(err.message);
+  });
 };
 
 
