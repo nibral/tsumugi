9386581d2678a1ac3778a6ad117cc3e227c37a69
nibral <nibral@users.noreply.github.com>
2015-09-19 15:47:17 +0900

    番組情報をJSONで出力できるようになった

--- schedule.js
+++ schedule.js
@@ -1,8 +1,15 @@
-var url = 'http://www.agqr.jp/timetable/streaming.php';
-
 var http = require('http')
 var cheerio = require('cheerio');
 
+// 番組表
+// 0:月曜 6:日曜
+var programs = [];
+for (var i = 0; i < 7; i++) {
+    programs[programs.length] = {};
+}
+
+// 番組表データ取得
+var url = 'http://www.agqr.jp/timetable/streaming.php';
 http.get(url, function (res) {
     // 文字エンコードを明示しないと、マルチバイト文字がchunkを跨いで
     // 届いた時に文字化けする
@@ -27,53 +34,48 @@ http.get(url, function (res) {
             /<\/tr>[\s]*<th class="time3" rowspan="2">2/g,
             '</tr>\n<tr>\n<th class="time3" rowspan="2">2'
             );
-            
-        // cheerioでhtmlを解析
-        var $ = cheerio.load(body); 
 
         // 前枠継続フラグ
         var rowskip = [0, 0, 0, 0, 0, 0, 0];    
 
         // 30分ごとの枠
+        var $ = cheerio.load(body);
         $('.schedule-ag > table > tbody > tr').each(function () {
-            // 時間ヘッダ
-            console.log($(this).children('th').text() + '--------');
-
-            // 曜日カウンタ        
-            var day = 0;                            
-
             // 各曜日
+            var day = 0;
             $(this).children('td').each(function () {
                 // 前枠が継続する列をスキップ
                 while (rowskip[day] > 0) {
-                    console.log('    ' + rowskip[day] + ':');
                     day++;
                 }
-
                 // rowspanが未定義(=30分番組)なら1
                 var rowspan = $(this).attr('rowspan');
                 rowskip[day] = rowspan = rowspan ? rowspan : 1;
-                
-                // 各番組タイトル
-                $(this).children('.title-p').each(function () {
-                    var title = $(this).text().replace(/\n/g, '')
-                    title = title.replace(/[\s]*/g, '');
-                    console.log('    ' + rowspan + ':' + title);
-                });
+
+                // 初回放送と生放送のみ登録
+                if ($(this).attr('class')) {
+                    // 番組情報取得
+                    var time = $(this).children('.time').text();
+                    time = time.replace(/\n/g, '').replace(/[\s]*/g, '');
+                    var title = $(this).children('.title-p').text();
+                    title = title.replace(/\n/g, '').replace(/[\s]*/g, '');
+                    var rp = $(this).children('.rp').text();
+                    rp = rp.replace(/\n/g, '').replace(/[\s]*/g, '');
+
+                    if (time && title && rp) {
+                        programs[day][time] = { title: title, rp: rp };
+                    }
+                }
 
                 day++;
             });
-
-            // 前枠の継続する列が残ってたら埋める
-            while (day <= 6) {
-                console.log('    ' + rowskip[day] + ':')
-                day++;
-            }
             
             // 次枠へ
             for (var i = 0; i < rowskip.length; i++) {
                 rowskip[i]--;
             }
         });
+
+        console.log(JSON.stringify(programs, null, '    '));
     });
 });
