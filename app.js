2cbbe024bdf6745e66863e5f23a2710fc36b6379
nibral <nibral@dancingstarson.me>
2016-02-27 22:26:39 +0900

    番組表の形式を変更 & 取得処理をPromise化 #4 #5

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+// 超!A&G番組表取得
+const agqr = require('./lib/agqr');
+let timetable = null;
+agqr.downloadAndParseTimetable().then((response) => {
+    timetable = response;
+}).catch((error) => {
+    console.log(error);
+    process.exit(1);
+});
+
+// ルーティング
+app.get('/', (request, response) => {
+    response.setHeader('Content-Type', 'text/plain');
+    response.send(JSON.stringify(timetable, null, '  '));
+});
+app.get('/next', (request, response) => {
+    try {
+        response.json(agqr.getProgramInfo());
+    } catch (error) {
+        response.send(error.message);
+    }
+});
+
+// サーバ起動
+const listenPort = process.env.PORT || 3000;
+app.listen(listenPort, () => {
+    console.log('start listening on port %d', listenPort);
+});
